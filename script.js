f45a12419ac3f51e34d03d695a59100c71c041d9
Khalil Meftah <khalile.meftah@gmail.com>
2022-09-30 15:48:20 +0100

    Added checkbox and delete button
    added event listner to checkbox, added delete button but still need to make it functionnal

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 
 let todos = document.getElementById('todos');
 let input = document.getElementById('input');
-
-
+let lis = document.querySelectorAll('li');
+let classNum = 0;
 function addTask(){
-    let li = document.createElement('li');
-    let check = document.createElement('input');
-    check.setAttribute('type', 'checkbox');
     if (input.value !== ""){
-        li.textContent = input.value;
-        todos.appendChild(check); 
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+        let check = document.createElement('input');
+        //let modif = document.createElement('button');
+        let supp = document.createElement('button');
+
+        li.setAttribute('class', 'li'+classNum);
+        span.setAttribute('class', 'li'+classNum);
+        check.setAttribute('class', 'li'+classNum);
+        //modif.setAttribute('class', 'li'+classNum);
+        supp.setAttribute('class', 'li'+classNum);
+
+        //modif.textContent = "Modfier";
+        supp.textContent = "X";
+        span.textContent = input.value;
+
+        check.setAttribute('type', 'checkbox')
+        check.addEventListener('change', function() {
+            if (this.checked) {
+              span.style.textDecoration = "line-through";
+            } else {
+                span.style.textDecoration = "none";
+            }
+          });
+
+        //modif.addEventListener('click', modifyTask);
+        
+
+        
+        li.appendChild(check); 
+        li.appendChild(span); 
+        //li.appendChild(modif); 
+        li.appendChild(supp); 
         todos.appendChild(li);
+
+        supp.addEventListener('click', deleteTask(li));
+        classNum += 1;
     } else {
         console.log('No task is given!');
     }
@@ -18,9 +49,16 @@ function addTask(){
 }
 
 function clearAll(){
-    let lis = document.querySelectorAll('li');
+    
     for(i=0; i<lis.length; i++){
         lis[i].remove();
     }
     
+}
+
+function modifyTask(){
+    
+}
+function deleteTask(row){
+    row.remove();
 }
